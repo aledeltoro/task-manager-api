ea7a62af95a14d2e536dcc21eabe3851d8433031
aledeltoro <alejandroencarnacion1@gmail.com>
2020-06-17 23:01:00 -0400

    feat: Add multiple changes to User model Refactor user object to Mongoose schema Add pre hook method for saving a document Add model method to find user credentials Add instance method to generate auth token

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -9,6 +11,7 @@ const User = mongoose.model("User", {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,7 +41,54 @@ const User = mongoose.model("User", {
       }
     },
   },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    }
+  }],
 });
 
-// We need to export our model just so it can be used in other parts of our code
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() }, "secretkey");
+ 
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+// Statics methods are accessible through the model. These are also referred to as "model methods"
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+// Hash plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
 module.exports = User;
